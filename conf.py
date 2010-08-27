7c73af80ab4b42e76c29d42d260bb95f416c5bae
Michael Ziegler <diese-addy@funzt-halt.net>
2010-08-27 14:57:51 +0200

    new tempalte

--- conf.py
+++ conf.py
@@ -96,12 +96,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme="agogo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = { 'bgcolor': 'white' }
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
